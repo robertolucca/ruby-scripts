9682ea14d2eacbd8b5c48c8ea804afe53652047f
robertolucca <roberto_lucca@yahoo.com>
2015-06-07 10:07:35 -0700

    rest updated

--- rest-client.rb
+++ rest-client.rb
@@ -1,27 +1,58 @@
-require 'net/http'
-require 'json'
+require 'net/http'  #standard ruby library
+require 'json'      #gem install json
+
+#ansi escape sequences
+#from http://bluesock.org/~willkg/dev/ansi.html#sequences
+BLUE_BOLD = "\e[34;1m"
+NORMAL = "\e[0m"
+RED = "\e[31m"
+CLEAR_SCREEN = "\e[2J"
 
 url='http://localhost:4567/FamilyMembers'
-
-while true
-  print "\n\e[31ms\e[0mearch \e[31ml\e[0mist \e[31ma\e[0mdd \e[31mu\e[0mpdate \e[31md\e[0melete \e[31mq\e[0muit -> "
+#fancy colorized commands menu
+cmds='search list add update delete quit'.split(' ')
+cmds.each {|w| w.sub!(/([a-z])/,BLUE_BOLD+'\1'+NORMAL)}
+menu=cmds.join(' ')
+#enclose in begin/end to trap exceptions
+begin
+  print CLEAR_SCREEN
+  while true
+  print "\nSelect command: "+menu+' -> '
   resp=gets().chop!
   case(resp)
     when 'l' then
       resp = Net::HTTP.get_response(URI.parse(url))
       print resp.code,'  ',resp.message,"\n"
-      puts resp.body
+      if resp.code == '200'then
+        obj=JSON.parse(resp.body)
+        printf(BLUE_BOLD+"%-30s %-30s\n"+NORMAL,'Name','Birthday')
+        obj.each {|n| printf("%-30s %-30s\n",n[0],n[1])}
+      end
     when 's' then
-      print "Name   -> "
+      print "Enter name   -> "
       name=gets().chop!
-      exit if name == 'q'
-      puts name
       resp = Net::HTTP.get_response(URI.parse(url+'/'+name))
       print resp.code,'  ',resp.message,"\n"
-      puts resp.body
+      if resp.code == '200' then
+        printf("%s \t\t%s\n",name,resp.body)
+      end
+    when 'a' then
+      print "Enter name -> "
+      name=gets().chop!
+      print "Enter birth date  -> "
+      dob=gets().chop!
+      resp = Net::HTTP.post_form(URI.parse(url),'name' => name, 'dob' => dob)
+      print resp.code,'  ',resp.message,"\n"
     when 'q' then
       exit
-  end
-  #obj=JSON.parse(resp.body)
-  #p obj["results"][0]["formatted_address"]
+    else 
+	    puts RED+'Invalid command !'+NORMAL
+    end
 end
+
+rescue SystemCallError      #Top level catch-all
+#rescue Errno::ECONNREFUSED #Connection refused
+  print  $!
+  abort
+end
+
--- rest-server.rb
+++ rest-server.rb
@@ -7,13 +7,22 @@ birthdates = {
 'Veronica'=>'June 24, 1973',
 'Roberto'=>'April 30, 1959' }
 
-
+#search
 get '/FamilyMembers/:name' do
   res=birthdates[params[:name]]
   return status 404 if res.nil?
-  res.to_json 
+  res
 end
 
+#list
 get '/FamilyMembers' do
   birthdates.to_json 
+end
+
+#add
+post '/FamilyMembers' do
+  res=birthdates[params['name']]
+  return status 409 if !res.nil?
+  birthdates[params['name']]=params['dob']
+  status 201
 end
